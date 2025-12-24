1e838acf256009c3ae4f0c090aef917032a7dc79
RickyRicardo787 <rickyserrano904@outlook.com>
2025-12-23 20:30:22 -0500

    force light mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="light">
       <body className="min-h-screen bg-background text-foreground">
         <Navbar />
 
